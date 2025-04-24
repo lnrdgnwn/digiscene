014f7f3e8588367968cb3a6f46256151605e61ad
lnrdgnwn <leonardogunawan15@gmail.com>
2025-04-24 14:29:26 +0700

    feat: add state management and logic in App.jsx for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Header from "./components/Header"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [nfcStatus, setNfcStatus] = useState(false);
+  const [nfcData, setNfcData] = useState({
+     nama: "",
+     nim: "",
+     jurusan: "",
+  });
+
+  const handleNfcData = (nfcData) => {
+    setNfcData(nfcData);
+  }
+
+  const handleNfcStatusChange = (newStatus) => {
+    setNfcStatus(newStatus);
+  }
+
+  console.log(nfcStatus);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="relative flex flex-col items-center">
+      <Header nfcStatus={nfcStatus} />
+        {nfcStatus ? <Form nfcStatus ={nfcStatus} nfcData={nfcData} /> : <Nfc onChangeNfcStatus={handleNfcStatusChange} onScan={handleNfcData} />} 
+    </div>
   )
 }
 
