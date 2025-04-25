5422139d8605910d36dde136a7762aa86a9d02ac
lnrdgnwn <leonardogunawan15@gmail.com>
2025-04-25 14:01:16 +0700

    feat(nfc) : add nfc and insert img

---
+++ src/components/NFC.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+
+function Nfc({ onChangeNfcStatus, onScan }) {
+  const [nfcError, setNfcError] = useState(false);
+
+  const changeNfcStatus = (newStatus) => {
+    onChangeNfcStatus(newStatus);
+  };
+
+  const changeNfcData = ({ nama, nim, jurusan }) => {
+    onScan({ nama, nim, jurusan });
+  };
+
+  const parseAndSend = (data) => {
+    const regex = /Nama:\s*(.*?)\s*NIM:\s*(\d+)\s*Jurusan:\s*(.+)/s;
+    const match = data.match(regex);
+
+    if (match) {
+      const [_, nama, nim, jurusan] = match;
+
+      changeNfcData({
+        nama: nama.trim(),
+        nim: nim.trim(),
+        jurusan: jurusan.trim(),
+      });
+
+      changeNfcStatus(true);
+      setNfcError(false); // Reset error
+    } else {
+      setNfcError(true);
+    }
+  };
+
+  const handleNfcRead = async () => {
+    setNfcError(false); // Reset error di awal klik
+
+    if (!("NDEFReader" in window)) {
+    //   Fallback ke dummy data jika NFC tidak didukung
+      const dummyData = `Nama: Darrell Satriano\nNIM: 09021282328049\nJurusan: Teknik Informatika`;
+      parseAndSend(dummyData);
+    setNfcError(true);
+      return;
+    }
+
+    try {
+      const reader = new NDEFReader();
+      await reader.scan();
+
+      reader.onreading = (event) => {
+        let nfcData = "";
+        for (const record of event.message.records) {
+          const textDecoder = new TextDecoder();
+          nfcData += textDecoder.decode(record.data) + "\n";
+        }
+
+        parseAndSend(nfcData);
+      };
+
+      reader.onerror = () => {
+        setNfcError(true);
+      };
+    } catch (error) {
+      console.error("Gagal scan NFC:", error);
+      setNfcError(true);
+    }
+  };
+
+  
+  return (
+    <div className="absolute top-60 w-full px-7 flex items-center justify-center">
+      <div className="flex flex-col w-full justify-center items-center rounded-[12px] bg-white shadow-2xl">
+        <div className="relative w-full flex justify-center items-center p-6">
+        {nfcError ? 
+         <img src="img/whiteNFC.png" alt="NFC Icon" className="transition-all duration-300 blur-[2px]"/>
+         : 
+         <img src="img/NFC.png" alt="NFC Icon" className="transition-all duration-300"/>}
+
+          {nfcError && (
+            <div className="absolute inset-0 flex items-center px-3  justify-center text-red-500">
+              <p className=" text-center text-2xl bg-white/60 font-semibold border border-red-500 py-3 rounded-md text-red-500">
+                NFC tidak didukung atau gagal membaca!
+              </p>
+            </div>
+          )}
+        </div>
+
+        <button
+          onClick={handleNfcRead}
+          className="w-full text-center font-bold cursor-pointer text-white text-2xl bg-blue-950 px-6 py-3 rounded-md"
+        >
+          Scan Sekarang
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Nfc;
