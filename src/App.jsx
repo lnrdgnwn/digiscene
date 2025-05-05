a5d2a03ac867803dec6798d2c606df8455de4db3
lnrdgnwn <leonardogunawan15@gmail.com>
2025-05-05 22:07:48 +0700

    refactor: adding time format

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header"
+import Nfc from "./components/NFC";
+import Form from "./components/Form"
 
 function App() {
   const [nfcStatus, setNfcStatus] = useState(false);
@@ -8,6 +10,39 @@ function App() {
      nim: "",
      jurusan: "",
   });
+  const [time, setTime] = useState("");
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    const updateDateTime = () => {
+      const now = new Date();
+      const formattedTime = now.toLocaleTimeString("id-ID").replace(/\./g, ":");
+      const formattedDate = now.toLocaleDateString("id-ID", {
+        timeZone: "Asia/Jakarta",
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+  
+      setTime(formattedTime);
+      setDate(formattedDate);
+
+    }
+
+    updateDateTime();
+
+    const now = new Date();
+    const delay = (60 - now.getSeconds()) * 1000;
+  
+    const timeout = setTimeout(() => {
+      updateDateTime();
+      const interval = setInterval(updateDateTime, 60000);
+      return () => clearInterval(interval);
+    }, delay);
+  
+    return () => clearTimeout(timeout);
+  }, []);
 
   const handleNfcData = (nfcData) => {
     setNfcData(nfcData);
@@ -21,8 +56,8 @@ function App() {
 
   return (
     <div className="relative flex flex-col items-center">
-      <Header nfcStatus={nfcStatus} />
-        {nfcStatus ? <Form nfcStatus ={nfcStatus} nfcData={nfcData} /> : <Nfc onChangeNfcStatus={handleNfcStatusChange} onScan={handleNfcData} />} 
+      <Header nfcStatus={nfcStatus} time ={time.substring(0, 5)} date={date} />
+        {nfcStatus ? <Form nfcStatus ={nfcStatus} nfcData={nfcData} time={time} date={date} /> : <Nfc onChangeNfcStatus={handleNfcStatusChange} onScan={handleNfcData} />} 
     </div>
   )
 }
