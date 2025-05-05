182bc819172ea2a4c86eb5d74e09817aa705b39d
lnrdgnwn <leonardogunawan15@gmail.com>
2025-05-05 22:36:59 +0700

    fix img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header({ nfcStatus, time, date }) {
         {nfcStatus ? 
         // Kondisi nfcStatus = true
         <header className="relative w-full">
-            <div className="relative bg-[url(img/bluePaper.png)] text-white flex justify-between items-center px-7 pt-12 pb-56 rounded-b-[100px]">
+            <div className="relative  text-white flex justify-between items-center px-7 pt-12 pb-56 rounded-b-[100px]" style={{ backgroundImage: "url('/img/bluePaper.png')" }}>
                 <h1 className="font-bold text-xl">DigiScene</h1>
                 <div className="flex flex-col items-end">
                     <h2 className="text-2xl font-bold">{time}</h2>
@@ -18,7 +18,7 @@ function Header({ nfcStatus, time, date }) {
         : 
         // Kondisi nfcStatus = false
         <header className="relative w-full">
-             <div className="bg-[url(img/bluePaper.png)] flex flex-col items-center justify-center text-white rounded-b-[100px]">
+             <div className=" flex flex-col items-center justify-center text-white rounded-b-[100px]" style={{ backgroundImage: "url('/img/bluePaper.png')" }}>
                 <p className="text-xl font-poppins pt-10 pb-3">Selamat Datang di <span className="font-bold">DigiSence</span></p>
                 <div className="flex flex-col items-center justify-center pb-42">
                     <h2 className="text-8xl font-bold">{time}</h2>
